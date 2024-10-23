5dd9b927d872650899651702d1fdbfb7fcc6fbfc
20210298 <144835269+20210298@users.noreply.github.com>
2024-10-23 16:18:51 +0900

    start

---
+++ src/test/java/kr/ac/kumoh/s202102298/w24w09javalotto/W24w09javalottoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s202102298.w24w09javalotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W24w09javalottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
